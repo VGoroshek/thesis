1f4102d79613dd66ee3184eab76f2ab2dd9ad066
dagkesamanskaya <dagkesamanskaya@gmail.com>
2019-11-19 15:09:50 +0300

    Power Set Library for StrategySubarrayArray

---
+++ PowerSetLibrary/PSLibraryFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PowerSetLibrary
+    {
+        public class PSLibraryFile
+        {
+
+            static T[][] FastPowerSet<T>(T[] seq)
+            {
+                var powerSet = new T[1 << seq.Length][]; // двоичный размер множества всех подмножеств
+                powerSet[0] = new T[0]; // первый в любом случае пустой
+                for (var i = 0; i < seq.Length; i++) 
+                {
+                    var cur = seq[i]; //элемент, что сейчас добавляем в подмножество
+                    Console.WriteLine(seq[i]);
+                    var count = 1 << i;
+                    for (var j = 0; j < count; j++)
+                    {
+                        var source = powerSet[j];
+                        var destination = powerSet[count + j] = new T[source.Length + 1];
+                        for (var q = 0; q < source.Length; q++)
+                            destination[q] = source[q];
+                        destination[source.Length] = cur;
+                    }
+                }
+                return powerSet;
+            }
+    }
+    }
+
